3024fdfb10a3e6f70a10cb636ce8e60f841b5513
Kirill <kirik201100@mail.ru>
2021-03-10 02:25:00 +0300

    Добавил Задание №9 (почти полностью)

--- num.rb
+++ num.rb
@@ -1,3 +1,4 @@
+require 'prime'
 #Задание №5
 def num n
   puts(n)
@@ -7,20 +8,16 @@ num 5
 #Задание №6
 def digits_sum n
   s=0
-  while !(n==0)
-    dig=n%10
-    s+=dig
-    n=n/10
+  for i in n.digits
+    s+=i
   end
   puts(print "Сумма цифр числа: ", s)
 end
 
 def digits_mult n #2
   m=1
-  while !(n==0)
-    dig=n%10
-    m*=dig
-    n=n/10
+  for i in n.digits
+    m*=i
   end
   puts(print "Произведение цифр числа: " , m)
 end
@@ -29,27 +26,14 @@ digits_sum 252
 digits_mult 589
 
 def max_dig n
-  max=n%10
-  n=n/10;
-  while !(n==0)
-    if ((n%10)>max)
-      max=n%10
-    end
-    n=n/10
-  end
-  puts(print "Максимальная цифра числа: ",max)
+  ma=n.digits.to_a.max
+
+  puts(print "Максимальная цифра числа: ",ma)
 end
 
 def min_dig n
-  min=n%10
-  n=n/10;
-  while !(n==0)
-    if ((n%10)<min)
-      min=n%10
-    end
-    n=n/10
-  end
-  puts(print "Минимальная цифра числа: ", min)
+  mi=n.digits.to_a.min
+  puts(print "Минимальная цифра числа: ", mi)
 end
 
 max_dig 12985
@@ -58,33 +42,26 @@ min_dig 583
 #Задание №7
 
 #Метод 1
-
+=begin
 def simple n
   for i in 2..Math.sqrt(n)
       return false   if n%i==0
   end
   return true
 end
-
+=end
 def simple_sum n
   sum=0
-  for i in 2..n/2
-    if (n%i==0&&simple(i)==true)
-      sum+=i
-    end
-  end
+  sum=n.prime_division.inject(0){|s, num| s+=num[0]}
   puts (print "Сумма простых делителей числа: ", sum)
 end
-simple_sum 6
+simple_sum 30
 
 #Метод 2
 
 def counter n
-  k=0
-  for i in n.digits
-    k += (!(i%2==0)&&i>3) ? 1 : 0
-  end
-  puts(print "Количество нечетных цифр числа, больших 3: ", k)
+    k = n.digits.find_all {|elem| (elem.odd?)&&(elem>3)}
+  puts(print "Количество нечетных цифр числа, больших 3: ", k.size)
 end
 counter 55
 
