d4bf1b516ec9a83df0e5c6bceff7297bb125ee25
Kirill <kirik201100@mail.ru>
2021-03-11 18:14:43 +0300

    Добавил Задание №2 (ЛР№2)

--- lab_2.rb
+++ lab_2.rb
@@ -1,40 +1,25 @@
-#Задание №1
+#Задание #2
 puts "Введите количество элементов массива и сами элементы"
 arr=Array.new(gets.to_i){|i| gets.to_i}
+puts 
 def sum arr
-  s=0
-  for i in 0...arr.size
-    s+=arr[i]
-  end
+  s=arr.inject {|sum, elem| sum+=elem}
 puts(print "Сумма элементов массива = ", s)
 end
 
 def mult arr
-  m=1
-  for i in 0...arr.size
-    m*=arr[i]
-  end
+  m=arr.inject(1) {|prod, elem| prod*=elem}
 puts(print "Произведение элементов массива = ", m)
 end
 
 def max arr
-  max=-1
-  for i in 0...arr.size
-    if (arr[i]>max)
-      max=arr[i]
-    end
-  end
+  max=arr.max
   puts(print "Максимальный элемент массива = ", max)
   end
 
 def min arr
-  min=100000000
-  for i in 0...arr.size
-    if (arr[i]<min)
-      min=arr[i]
-      end
-    end
-    puts(print "Минимальный элемент массива = ", min)
+  min=arr.min
+  puts(print "Минимальный элемент массива = ", min)
     end
 
 sum arr
