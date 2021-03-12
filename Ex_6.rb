c133d580dce69c576ce6b42fd58c9b54fb23344b
Kirill <kirik201100@mail.ru>
2021-03-12 16:36:02 +0300

    Добавил ЛР№2 Задание №6 (Задача 13)

--- Ex_6.rb
+++ Ex_6.rb
@@ -24,3 +24,27 @@ def count_after_max
     puts k
   end
 end
+
+#Задача 13
+
+def replace
+  puts "Введите количество элементов массива: "
+  size=gets.to_i
+  puts "Введите элементы массива: "
+  arr=Array.new(size) {|i| gets.to_i}
+  puts(print arr)
+  min=1000000
+  res=[]
+  for i in 0...arr.size
+    if (arr[i]<min)
+      min=arr[i]
+      min_index=i
+    end
+  end
+  for j in 0...arr.size
+    if (j<min_index)
+      arr.push(arr.shift)
+    end
+  end
+  puts(print arr)
+end
