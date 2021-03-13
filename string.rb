e141aceeb4f8781cfc7b2ca6ca607c47023c7e84
Kirill <kirik201100@mail.ru>
2021-03-13 21:38:07 +0300

    Добавил ЛР№2 Задание №9 (Задача 9)

--- string.rb
+++ string.rb
@@ -13,3 +13,15 @@ def rus
   amount=str.scan(/[А-я]/).size
   puts(print "Количество русский букв в строке #{str} = ", amount)
 end
+
+#Задача 9
+
+def palindrom
+  str="abba А РОЗА УПАЛА НА ЛАПУ АЗОРА"
+  check=str.scan(/[a-zа-я]/)
+  if (check==check.reverse)
+    puts "Строчные буквы строки '#{str}' образуют палиндром"
+  else
+    puts "Строчные буквы строки '#{str}' не образуют палиндром"
+  end
+end
