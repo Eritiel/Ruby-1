0859adbdc011e619ec649669c2b512b2bbfe69e1
Kirill <kirik201100@mail.ru>
2021-03-13 20:49:33 +0300

    Добавил ЛР№2 Задание №9 (Задача 1)

--- string.rb
+++ string.rb
@@ -1,5 +1,15 @@
 #Задание №8
 
 s=String.new(str="5 8 99 4 55 6 1 9 3")
-res=s.split(" ").each{|i| i.to_i}.max
-puts res
+max=s.split(" ").each{|i| i.to_i}.max
+puts(print "Максимальное значение в строке #{s} = ", max)
+
+#Задание №9
+
+#Задача 1
+
+def rus
+  str="ghfkgjаопщщкрерлg"
+  amount=str.scan(/[А-я]/).size
+  puts(print "Количество русский букв в строке #{str} = ", amount)
+end
