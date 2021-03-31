e5e225508357c161f251b8e5b96530ac2b5c72f4
Kirill <kirik201100@mail.ru>
2021-04-01 00:52:10 +0300

    Добавил ЛР№3 Задание 8

--- lab_3.rb
+++ lab_3.rb
@@ -1,10 +1,8 @@
 class Employee
-  attr_reader :last_job,:last_job_salary,:phone,:mail,:surname,:first_name,:fath,:birth,:passport
+  attr_reader :last_job,:last_job_salary,:phone,:mail,:name,:birth,:passport
   attr_accessor :address,:spec,:job_name,:exp
-  def initialize (surname, first_name, fath, birth, phone, address, mail, passport, spec, exp, last_job, job_name, last_job_salary)
-    @surname=surname
-    @first_name=first_name
-    @fath=fath
+  def initialize (name, birth, phone, address, mail, passport, spec, exp, last_job, job_name, last_job_salary)
+    @name=name
     @birth=birth
     @phone=phone
     @address=address
@@ -21,23 +19,26 @@ class Employee
       @last_job_salary=new_last_job_salary
     end
   end
-  def phone_setter(new_phone)
+  def phone_setter()
+    new_phone=gets.chomp
     @phone=new_phone if phone_format(new_phone)
   end
-  def email_setter(new_mail)
+  def email_setter()
+    new_mail=gets.chomp()
     @mail=new_mail if email_format(new_mail)
   end
-  def name_setter(new_surname, new_first_name, new_fath)
-    if name_format(surname, first_name, fath)
-      @surname=new_surname
-      @first_name=new_first_name
-      @fath=new_fath
+  def name_setter()
+    new_name=gets.chomp
+    if name_format(new_name)
+      @name=new_name
     end
   end
-  def birth_date_setter(new_birth)
+  def birth_date_setter()
+    new_birth=gets.chomp
     @birth=new_birth if date_format(new_birth)
   end
-  def passport_setter(new_passport)
+  def passport_setter()
+    new_passport=gets.chomp
     @passport=new_passport if passport_format(new_passport)
   end
   def number_check(phone)
@@ -54,8 +55,8 @@ class Employee
       return false
     end
   end
-  def name_check(surname, first_name, fath)
-    if (surname=~/[\s]*[а-яА-Яa-zA-Z]+[\s]*[-]{0,1}[\s]*[a-zA-ZА-Яа-я]*[\s]*/&&first_name=~/[\s]*[а-яА-Яa-zA-Z]+[\s]*[-]{0,1}[\s]*[a-zA-ZА-Яа-я]*[\s]*/&&fath=~/[\s]*[а-яА-Яa-zA-Z]+[\s]*[a-zA-ZА-Яа-я]*[\s]*/)
+  def name_check(name)
+    if name=~/^(?:[а-яА-ЯA-Za-z]+|[а-яА-ЯA-Za-z]+[\s]*-[\s]*[a-zА-Яа-яA-Z]+)[\s]+(?:[а-яА-ЯA-Za-z]+|[а-яА-ЯA-Za-z]+[\s]*-[\s]*[a-zА-Яа-яA-Z]+)[\s]*(?:[а-яА-ЯA-Za-z]+|[а-яА-ЯA-Za-z]+[\s]*[a-zА-Яа-яA-Z]+[\s]*)$/
       return true
     else
       return false
@@ -91,15 +92,14 @@ class Employee
       puts print "E-mail: ", s
     end
   end
-  def name_format(surname, first_name, fath)
-    if name_check(surname, first_name, fath) == false
+  def name_format(name)
+    if name_check(name) == false
       raise StandardError.new "Неверный формат имени"
     else
-      s=surname.delete(" ").split("-").map{|i| i.capitalize}.join("-")
-      f=first_name.delete(" ").split("-").map{|i| i.capitalize}.join("-")
-      o=fath.split(" ")
-      puts print "Телефон сотрудника: ",s," ",f," ",o.first.capitalize, " ", o.last
-    end
+      s = name.downcase
+      s = name.gsub(/\s\s+/, " ").gsub(/\b./, &:upcase).gsub(/(?<=[а-яa-zA-ZА-Я])\s-\s(?=[а-яa-zA-ZА-Я])/, "-")
+      end
+      puts print "Имя сотрудника: ",s
   end
   def date_format(birth)
     if birth_date_check(birth) == false
@@ -119,12 +119,12 @@ class Employee
     end
   end
   def output()
-    name_setter(surname, first_name, fath)
-    birth_date_setter(birth)
-    phone_setter(phone)
+    name_setter()
+    birth_date_setter()
+    phone_setter()
     puts print "Адрес: ", address
-    email_setter(mail)
-    passport_setter(passport)
+    email_setter()
+    passport_setter()
     puts print "Специальность: ", spec
     puts print "Название должности: ", job_name
     puts print "Стаж работы, прошлое место работы, зарплата на прошлом месте работы: ", exp,", ", last_job,", ",last_job_salary unless exp==0
@@ -132,9 +132,26 @@ class Employee
 end
 
 class TestEmployee<Employee
+  def menu()
+    puts "Выберите поле, которое хотите ввести: "
+    line=gets.chomp
+    case line
+    when "FIO"
+      name_setter()
+    when "Date of birth"
+      birth_date_setter()
+    when "Phone"
+      phone_setter()
+    when "E-mail"
+      email_setter()
+    when "Passport"
+      passport_setter()
+    end
+  end
+
 end
 
-emp1 = TestEmployee.new("Темный","Кирилл" ,"Владиславович","2.11.2000","79608635782","Сормовская","kirik201100@mail.ru","0315 02 32 62","Программистер",5,"Макдак","Уборщик","5000")
-emp2 = TestEmployee.new("Лупа","Пупа" ,"Ахмед соглы","11.10.2000","79608642149","Тюляева","beep_boop@gmail.com","03 20 02-37-59","Лингвист",0,"КФС","Кассир","7000")
+emp1 = TestEmployee.new("Темный Кирилл Владиславович","2.11.2000","79608635782","Сормовская","kirik201100@mail.ru","0315 02 32 62","Программистер",5,"Макдак","Уборщик","5000")
+emp2 = TestEmployee.new("Лупа - Пупа пупа - пупа ахмед соглы","11.10.2000","79608642149","Тюляева","beep_boop@gmail.com","03 20 02-37-59","Лингвист",0,"КФС","Кассир","7000")
 emp1.output()
-emp2.output()
+emp1.menu()
