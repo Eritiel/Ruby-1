e647c76347626b13351e1e1a98b4cad2082dd61f
Kirill <kirik201100@mail.ru>
2021-04-03 01:54:29 +0300

    Добавил ЛР№3 Задание 9 и немного изменил прошлые задания

--- lab_3.rb
+++ lab_3.rb
@@ -2,12 +2,12 @@ class Employee
   attr_reader :last_job,:last_job_salary,:phone,:mail,:name,:birth,:passport
   attr_accessor :address,:spec,:job_name,:exp
   def initialize (name, birth, phone, address, mail, passport, spec, exp, last_job, job_name, last_job_salary)
-    @name=name
-    @birth=birth
-    @phone=phone
-    @address=address
-    @mail=mail
-    @passport=passport
+    self.name=name
+    self.birth=birth
+    self.phone=phone
+    self.address=address
+    self.mail=mail
+    self.passport=passport
     @spec=spec
     @job_name=job_name
     self.check_exp(exp,last_job,last_job_salary)
@@ -19,27 +19,20 @@ class Employee
       @last_job_salary=new_last_job_salary
     end
   end
-  def phone_setter()
-    new_phone=gets.chomp
-    @phone=new_phone if phone_format(new_phone)
+  def phone=(new_phone)
+    @phone=phone_format(new_phone)
   end
-  def email_setter()
-    new_mail=gets.chomp()
-    @mail=new_mail if email_format(new_mail)
+  def mail=(new_mail)
+    @mail=email_format(new_mail)
   end
-  def name_setter()
-    new_name=gets.chomp
-    if name_format(new_name)
-      @name=new_name
-    end
+  def name=(new_name)
+      @name=name_format(new_name)
   end
-  def birth_date_setter()
-    new_birth=gets.chomp
-    @birth=new_birth if date_format(new_birth)
+  def birth=(new_birth)
+    @birth=date_format(new_birth)
   end
-  def passport_setter()
-    new_passport=gets.chomp
-    @passport=new_passport if passport_format(new_passport)
+  def passport=(new_passport)
+    @passport=passport_format(new_passport)
   end
   def number_check(phone)
     if phone=~/^(\+?[7]|[7])([-+()]*[0-9]{3}[-+()]*[0-9]{7}|[0-9]{10}|[-+()]*[0-9]{3}[-+()]*[-+()]*[0-9]{3}[-+()]*[0-9]{4}|[-+()]*[0-9]{3}[-+()]*[-+()]*[0-9]{3}[-+()]*[0-9]{2}[-+()]*[0-9]{2})\b/
@@ -81,7 +74,7 @@ class Employee
       raise StandardError.new "Неверный формат телефона"
     else
       s=phone.scan(/[0-9]/)
-      puts print "Телефон сотрудника: ",[s[0],s[1,3].join(),s[4,10].join()].join("-")
+      return [s[0],s[1,3].join(),s[4,10].join()].join("-")
     end
   end
   def email_format(mail)
@@ -89,7 +82,7 @@ class Employee
       raise StandardError.new "Неверный формат электронной почты"
     else
       s=mail.chars.map{|i| i.downcase}.join
-      puts print "E-mail: ", s
+      return s
     end
   end
   def name_format(name)
@@ -99,14 +92,14 @@ class Employee
       s = name.downcase
       s = name.gsub(/\s\s+/, " ").gsub(/\b./, &:upcase).gsub(/(?<=[а-яa-zA-ZА-Я])\s-\s(?=[а-яa-zA-ZА-Я])/, "-")
       end
-      puts print "Имя сотрудника: ",s
+      return s
   end
   def date_format(birth)
     if birth_date_check(birth) == false
       raise StandardError.new "Неверный формат даты рождения"
     else
       s=birth.split(".").map{|i| i.size<2 ? "0"+i : i}.join(".")
-      puts print "Дата рождения: ", s
+      return s
     end
   end
   def passport_format(passport)
@@ -115,21 +108,23 @@ class Employee
     else
       s=passport.delete("-").delete(" ").chars.first(4).join
       n=passport.delete("-").delete(" ").chars.last(6).join
-      puts print "Серия и номер паспорта: ", s, " ",n
+      return s,n
     end
   end
+
   def output()
-    name_setter()
-    birth_date_setter()
-    phone_setter()
+    puts print "ФИО сотрудника: ",name()
+    puts print "Дата рождения сотрудника: ",birth()
+    puts print "Телефон сотрудника: ",phone()
     puts print "Адрес: ", address
-    email_setter()
-    passport_setter()
+    puts print "E-mail: ",mail()
+    puts print "Серия и номер пасспорта: ", passport().join(" ")
     puts print "Специальность: ", spec
     puts print "Название должности: ", job_name
     puts print "Стаж работы, прошлое место работы, зарплата на прошлом месте работы: ", exp,", ", last_job,", ",last_job_salary unless exp==0
   end
 end
+
 
 class TestEmployee<Employee
   def menu()
@@ -137,21 +132,32 @@ class TestEmployee<Employee
     line=gets.chomp
     case line
     when "FIO"
-      name_setter()
-    when "Date of birth"
-      birth_date_setter()
+      puts "Введите имя сотрудника"
+      self.name=gets.chomp
+      puts print "ФИО сотрудника: ",name()
+    when "Birth"
+      puts "Введите дату рождения сотрудника"
+      self.birth=gets.chomp
+      puts print "Дата рождения сотрудника: ",birth()
     when "Phone"
-      phone_setter()
+      puts "Введите телефон сотрудника"
+      self.phone=gets.chomp
+      puts print "Телефон сотрудника: ",phone()
     when "E-mail"
-      email_setter()
+      puts "Введите e-mail сотрудника"
+      self.mail=gets.chomp
+      puts print "E-mail: ",mail()
     when "Passport"
-      passport_setter()
+      puts "Введите паспортные данные сотрудника"
+      self.passport=gets.chomp
+      puts print "Серия и номер пасспорта: ", passport().join(" ")
     end
   end
 
 end
 
-emp1 = TestEmployee.new("Темный Кирилл Владиславович","2.11.2000","79608635782","Сормовская","kirik201100@mail.ru","0315 02 32 62","Программистер",5,"Макдак","Уборщик","5000")
-emp2 = TestEmployee.new("Лупа - Пупа пупа - пупа ахмед соглы","11.10.2000","79608642149","Тюляева","beep_boop@gmail.com","03 20 02-37-59","Лингвист",0,"КФС","Кассир","7000")
+emp1 = TestEmployee.new("темный кирилл владиславович","2.11.2000","79608635782","Сормовская","kirik201100@mail.ru","03 15 02 32 62","Программистер",5,"Макдак","Уборщик","5000")
+emp2 = TestEmployee.new("Лупа - Пупа  пупа - пупа ахмед соглы","11.10.2000","79608642149","Тюляева","beep_boop@gmail.com","03 20 02-37-59","Лингвист",0,"КФС","Кассир","7000")
 emp1.output()
+emp2.output()
 emp1.menu()
